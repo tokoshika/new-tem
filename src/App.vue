3e0bc8ee3615c624cf0ae170a1677f1c23605053
tokoshika <103026403+tokoshika@users.noreply.github.com>
2022-12-20 22:02:08 +0900

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <header>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </header>
+
+  <section>
+    <div>
+      <router-view />
+      <Home />
+    </div>
+  </section>
+</template>
+
+<script>
+</script>
+
+<style>
+#app {
+  margin: 0;
+  padding: 0;
+}
+body {
+  background: rgb(231, 226, 226);
+  font-size: 32px;
+  text-align: center;
+  margin: 0;
+  padding: 0;
+}
+p {
+  margin: 0;
+}
+section {
+  margin-top: 0;
+  margin-left: 12%;
+  margin-right: 12%;
+  background: rgb(218, 195, 195);
+  margin-bottom: 0;
+}
+
+header {
+  margin: 0;
+  padding: 0;
+  z-index: 999;
+  width: 100%;
+  color: white;
+  background-color: black;
+  position: fixed;
+}
+</style>
---
+++ src/views/About.vue
@@ -0,0 +1,19 @@
+<template>
+  <section>
+    <br />
+    <br />
+    <p>this is About 1</p>
+    <p>this is About 2</p>
+    <p>this is About</p>
+    <p>this is About</p>
+    <p>this is About</p>
+    <p>this is About</p>
+    <p>this is About</p>
+  </section>
+</template>
+
+<style scoped>
+section {
+  /* margin-top: 50px; */
+}
+</style>
